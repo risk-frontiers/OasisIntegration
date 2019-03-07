662d2bb9078928888dcbd210444fba7a4f87e942
Sam Gamble <hexadessa@gmail.com>
2019-03-07 16:19:17 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 export OASIS_VER='1.0.0'
 export UI_VER='1.0.0'
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
 
 # SETUP AND RUN COMPLEX MODEL EXAMPLE
     # Reset compose file to last commit && update tag number 
+    cd $SCRIPT_DIR
     git checkout -- docker-compose.yml
-    sed -i 's|:latest|:${OASIS_VER}|g' $SCRIPT_DIR/$COMPOSE_PLATFORM
+    sed -i 's|:latest|:${OASIS_VER}|g' docker-compose.yml
 
     # reset and build custom worker
-    ggit checkout -- Dockerfile.custom_model_worker
-    sed -i "s|:latest|:${OASIS_VER}|g" $SCRIPT_DIR/Dockerfile.custom_model_worker
+    git checkout -- Dockerfile.custom_model_worker
+    sed -i "s|:latest|:${OASIS_VER}|g" Dockerfile.custom_model_worker
     docker build -f Dockerfile.custom_model_worker -t coreoasis/custom_model_worker:$OASIS_VER .
-
+    
     # Start API
     docker-compose up -d
 
@@ -21,6 +25,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     GIT_UI=OasisUI
     if [ -d $SCRIPT_DIR/$GIT_UI ]; then
         cd $SCRIPT_DIR/$GIT_UI
+        git fetch
         git checkout $UI_VER
     else
         git clone https://github.com/OasisLMF/$GIT_UI.git -b $UI_VER
@@ -29,7 +34,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     # Reset UI docker Tag
     cd $SCRIPT_DIR/$GIT_UI
     git checkout -- docker-compose.yml
-    sed -i 's|:latest|:${UI_VER}|g' $GIT_UI/docker-compose.yml
+    sed -i 's|:latest|:${UI_VER}|g' docker-compose.yml
     cd $SCRIPT_DIR
 
     # Start UI
