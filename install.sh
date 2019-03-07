daffb0813b306b70a7baeba7cbba2b33982779d5
Sam Gamble <hexadessa@gmail.com>
2019-03-07 16:04:28 +0000

    Update install script

--- install.sh
+++ install.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 export OASIS_VER='1.0.0'
-export UI_VER='1.0.0-rc1'
+export UI_VER='1.0.0'
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # SETUP AND RUN COMPLEX MODEL EXAMPLE
-    COMPOSE_PLATFORM='docker-compose.yml'
-    SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
     # Reset compose file to last commit && update tag number 
-    git checkout HEAD^ $COMPOSE_PLATFORM
+    git checkout -- docker-compose.yml
     sed -i 's|:latest|:${OASIS_VER}|g' $SCRIPT_DIR/$COMPOSE_PLATFORM
 
     # reset and build custom worker
-    git checkout HEAD^ Dockerfile.custom_model_worker
+    ggit checkout -- Dockerfile.custom_model_worker
     sed -i "s|:latest|:${OASIS_VER}|g" $SCRIPT_DIR/Dockerfile.custom_model_worker
     docker build -f Dockerfile.custom_model_worker -t coreoasis/custom_model_worker:$OASIS_VER .
 
@@ -29,8 +27,10 @@ export UI_VER='1.0.0-rc1'
     fi 
 
     # Reset UI docker Tag
-    git checkout HEAD^ $GIT_UI/docker-compose.yml
+    cd $SCRIPT_DIR/$GIT_UI
+    git checkout -- docker-compose.yml
     sed -i 's|:latest|:${UI_VER}|g' $GIT_UI/docker-compose.yml
+    cd $SCRIPT_DIR
 
     # Start UI
     docker network create shiny-net
