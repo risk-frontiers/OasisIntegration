e5f986a7643ca81e1c6b846cf39d0934d7fc62a4
Tahiry Rabehaja <tahiry.rabehaja@riskfrontiers.com>
2019-07-17 06:56:40 +1000

    dummy templates for other tests
    Signed-off-by: Tahiry Rabehaja <tahiry.rabehaja@riskfrontiers.com>

---
+++ tests/unit/GulcalcToBinTests.py
@@ -0,0 +1,13 @@
+import unittest
+
+from tests.unit.RFBaseTest import RFBaseTestCase
+
+
+class GulcalcToBinTests(RFBaseTestCase):
+    """This test case provides validation for the postcode lookup method
+    """
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/unit/PostcodeLookupTests.py
+++ tests/unit/PostcodeLookupTests.py
@@ -1,5 +1,13 @@
+import unittest
 
-class PostcodeLookupTests(RFBaseTest):
+from tests.unit.RFBaseTest import RFBaseTestCase
+
+
+class PostcodeLookupTests(RFBaseTestCase):
     """This test case provides validation for the postcode lookup method
     """
     pass
+
+
+if __name__ == '__main__':
+    unittest.main()
