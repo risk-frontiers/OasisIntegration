3e8156641da869fc632cbc524976460bb73bbece
Tahiry Rabehaja <tahiry.rabehaja@riskfrontiers.com>
2019-08-09 10:24:25 +1000

    nix
    Signed-off-by: Tahiry Rabehaja <tahiry.rabehaja@riskfrontiers.com>

--- rf_install.sh
+++ rf_install.sh
@@ -16,9 +16,9 @@ file_versions='data_version.json'
 
 cd ${SCRIPT_DIR}
 # Customize BATCH_COUNT in conf.ini
-# Ideally between 10 and 20 which represents the memory consumption by each independent RF .net engine
+# Ideally between 10 and 16 which represents the memory consumption by each independent RF .net engine
 # OASIS financial engine is quite memory intensive so we simply reserve twice as much memory at the moment
-SCALE_FACTOR=40 # = 20 * 2
+SCALE_FACTOR=16 # = 20 * 2
 BATCH_COUNT=$(expr `awk '/MemFree/ { printf "%.0f \n", $2/1024/1024 }' /proc/meminfo` / ${SCALE_FACTOR})
 BATCH_COUNT=$([ ${BATCH_COUNT} -le 1 ] && echo 1 || echo ${BATCH_COUNT})
 VCPU_COUNT=$(cat /proc/cpuinfo | awk '/^processor/{print $3}' | wc -l)
