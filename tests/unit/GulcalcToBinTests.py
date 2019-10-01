aaaf82e427fd206ef6a5019ae3bf7a2d6ac17efb
Tahiry Rabehaja <tahiry.rabehaja@riskfrontiers.com>
2019-10-01 14:43:54 +1000

    support for loss stream in python codes
    Signed-off-by: Tahiry Rabehaja <tahiry.rabehaja@riskfrontiers.com>

--- tests/unit/GulcalcToBinTests.py
+++ tests/unit/GulcalcToBinTests.py
@@ -6,9 +6,11 @@ import subprocess
 from backports.tempfile import TemporaryDirectory
 from parameterized import parameterized
 import pandas as pd
+import itertools
 
 from tests.unit.RFBaseTest import RFBaseTestCase
-from complex_model.GulcalcToBin import gulcalc_sqlite_to_bin
+from complex_model.GulcalcToBin import gulcalc_sqlite_to_bin, SUPPORTED_GUL_STREAMS
+from complex_model.Common import ArgumentOutOfRangeException
 
 
 TEST_DIR = os.path.dirname(__file__)
@@ -17,6 +19,7 @@ TEST_MODEL_DATA_DIR = os.path.join(TEST_DIR, 'data', 'model_data')
 
 
 def load_csv(con, csv_file, reg_col='coverage_id'):
+    """Loads a gul csv output from oasis gulcalc (or complex model gulcalc) into the sqlite table u_lossoasis_r254"""
     cur = con.cursor()
     cur.execute("CREATE TABLE u_lossoasis_r254 (event_id INTEGER, reg_id INTEGER, sample_id INTEGER, groundup REAL);")
 
@@ -28,13 +31,19 @@ def load_csv(con, csv_file, reg_col='coverage_id'):
     con.commit()
 
 
-def recreate_csv_from_bin(working_dir, file_fp):
+def recreate_csv_from_bin(working_dir, file_fp, stream_name="coverage"):
+    """This recreates an in memory sqlite database from an oasis gul csv output"""
     gul_fp = os.path.join(working_dir, "gul.bin")
 
     con = sqlite3.connect(":memory:")
-    load_csv(con, file_fp)
+    if stream_name in SUPPORTED_GUL_STREAMS:
+        reg_id = "coverage_id" if stream_name == "coverage" else "item_id"
+        load_csv(con, file_fp, reg_id)
+        stream_id = SUPPORTED_GUL_STREAMS[stream_name]
+    else:
+        raise ArgumentOutOfRangeException("Unsupported stream: " + stream_name)
     with open(gul_fp, 'wb') as gul_file:
-        gulcalc_sqlite_to_bin(con, gul_file, 1, 2)
+        gulcalc_sqlite_to_bin(con, gul_file, 1, stream_id)
     con.close()
 
     gul_csv_fp = os.path.join(working_dir, "gul.csv")
@@ -47,20 +56,22 @@ class GulcalcToBinTests(RFBaseTestCase):
     compatible with the current specification of oasis streams.
     """
 
-    @parameterized.expand([["test_1.csv".format(i)] for i in range(1, 4)] + [["gul.csv"]])
-    def test_event_ordering(self, test_filename):
+    @parameterized.expand([[i, s] for i, s in itertools.product(range(0, 4), list(SUPPORTED_GUL_STREAMS.keys()))])
+    def test_event_ordering(self, file_id, stream_type):
+        test_filename = "test_{}_{}.csv".format(file_id, stream_type)
         with TemporaryDirectory() as working_dir:
             file_fp = os.path.join(TEST_INPUT_DIR, test_filename)
-            gul_csv_fp = recreate_csv_from_bin(working_dir, file_fp)
+            gul_csv_fp = recreate_csv_from_bin(working_dir, file_fp, stream_type)
 
             expected = pd.read_csv(gul_csv_fp)
             self.assertTrue(pd.Index(expected["event_id"]).is_monotonic)
 
-    @parameterized.expand([["test_1.csv".format(i)] for i in range(1, 4)] + [["gul.csv"]])
-    def test_sidx_ordering(self, test_filename):
+    @parameterized.expand([[i, s] for i, s in itertools.product(range(0, 4), list(SUPPORTED_GUL_STREAMS.keys()))])
+    def test_sidx_ordering(self, file_id, stream_type):
+        test_filename = "test_{}_{}.csv".format(file_id, stream_type)
         with TemporaryDirectory() as working_dir:
             file_fp = os.path.join(TEST_INPUT_DIR, test_filename)
-            gul_csv_fp = recreate_csv_from_bin(working_dir, file_fp)
+            gul_csv_fp = recreate_csv_from_bin(working_dir, file_fp, stream_type)
 
             expected = pd.read_csv(gul_csv_fp)
             last_sid = -2
@@ -74,11 +85,12 @@ class GulcalcToBinTests(RFBaseTestCase):
                     self.assertLessEqual(last_sid, sidx)
                 last_sid = sidx
 
-    @parameterized.expand([["test_{}.csv".format(i)] for i in range(0, 4)] + [["gul.csv"]])
-    def test_multi_packets_stream(self, test_filename):
+    @parameterized.expand([[i, s] for i, s in itertools.product(range(0, 4), list(SUPPORTED_GUL_STREAMS.keys()))])
+    def test_multi_packets_stream(self, file_id, stream_type):
+        test_filename = "test_{}_{}.csv".format(file_id, stream_type)
         with TemporaryDirectory() as working_dir:
             file_fp = os.path.join(TEST_INPUT_DIR, test_filename)
-            gul_csv_fp = recreate_csv_from_bin(working_dir, file_fp)
+            gul_csv_fp = recreate_csv_from_bin(working_dir, file_fp, stream_type)
 
             with open(gul_csv_fp, "r") as gul_csv_file:
                 result = set(gul_csv_file.read().splitlines())
