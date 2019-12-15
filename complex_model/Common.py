695284acaf6f42d9fbadaa27c9e35b684d4e60b2
Tahiry Rabehaja <tahiry.rabehaja@riskfrontiers.com>
2019-12-16 10:24:57 +1100

    upgraded to hailaus 7.1
    Signed-off-by: Tahiry Rabehaja <tahiry.rabehaja@riskfrontiers.com>

--- complex_model/Common.py
+++ complex_model/Common.py
@@ -33,7 +33,7 @@ def oed_to_rf_peril(oed_peril_id):
 
 
 PerilSet = {
-    "hailaus": {"OED_CODE": 8192, "OED_ID": "XHL", "COUNTRY": "au", "MAX_EVENT_INDEX": 134704731},
+    "hailaus": {"OED_CODE": 8192, "OED_ID": "XHL", "COUNTRY": "au", "MAX_EVENT_INDEX": 134416537},
     "quakeaus": {"OED_CODE": 1, "OED_ID": "QEQ", "COUNTRY": "au", "MAX_EVENT_INDEX": 1000252},
     "floodaus": {"OED_CODE": 512, "OED_ID": "ORF", "COUNTRY": "au", "MAX_EVENT_INDEX": 535000},
     "fireaus": {"OED_CODE": 262144, "OED_ID": "BBF", "COUNTRY": "au", "MAX_EVENT_INDEX": 291577},
